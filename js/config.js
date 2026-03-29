d0a8ad062ea92d115338c858af68bb58b23954e1
ArisaFuziwara <77854460+ArisaFuziwara@users.noreply.github.com>
2026-03-29 15:02:27 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,10 @@
 export const CONFIG = {
-    apiKey: "AIzaSyCn6c3WgB2TfOj3tqXMiCdoL_83licJFm8",
-    authDomain: "edital-scout-c69d5.firebaseapp.com",
-    projectId: "edital-scout-c69d5",
-    storageBucket: "edital-scout-c69d5.firebasestorage.app",
-    messagingSenderId: "124108452953",
-    appId: "1:124108452953:web:c217791b81e013d2286d48"
-  };
+  apiKey: "AIzaSyCn6c3WgB2TfOj3tqXMiCdoL_83licJFm8",
+  authDomain: "edital-scout-c69d5.firebaseapp.com",
+  projectId: "edital-scout-c69d5",
+  storageBucket: "edital-scout-c69d5.firebasestorage.app",
+  messagingSenderId: "124108452953",
+  appId: "1:124108452953:web:c217791b81e013d2286d48"
+  },
+  limiteAnalisesMes: 3,
+};
